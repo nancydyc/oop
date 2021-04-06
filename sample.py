66576cb46d8f28a6d8488055e982dc9407819ba0
Yichen <msdaiyichen@gmail.com>
2021-04-06 12:38:27 -0700

    Modify apply_raise method to not return anything; Add subclass Developer

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Employee:
     raise_amount = 0.66
     number_of_emps = 0
@@ -14,7 +16,7 @@ class Employee:
         return self.first + " " + self.last
 
     def apply_raise(self):
-        return self.pay + self.raise_amount
+        self.pay = self.pay + self.raise_amount
 
     @classmethod
     def set_raise_amt(cls, amount):
@@ -25,6 +27,17 @@ class Employee:
         first, last, pay = emp_string.split("-")
         return cls(first, last, pay) # alternative constructors
 
+    @staticmethod # Use staticemethod when not touching self or cls
+    def is_worday(date):
+        if date.weekday() == 5 or date.weekday() == 6:
+            return False
+        return True
+
+class Developer(Employee):
+    raise_amount = 1111
+    pass
+
+
 emp_1 = Employee("Nancy", "Dai", 6666)
 print(emp_1.__dict__) # namespace for this instance
 print(emp_1.email) # preperty
@@ -32,10 +45,12 @@ print("Employee 1: ", emp_1.fullname()) # method
 
 print("====================================================================================")
 # use class variable
-print("Adjust raise for emp_1: ", emp_1.apply_raise())
+emp_1.apply_raise()
+print("Adjust raise for emp_1: ", emp_1.pay)
 emp_1.raise_amount = 0.88 # overwrite the classmethod
 print("Base raise apply to all employees (Class variable) is still: ", Employee.raise_amount)
-print("Adjust 2nd raise for emp_1: ", emp_1.apply_raise())
+emp_1.apply_raise()
+print("Adjust 2nd raise for emp_1: ", emp_1.pay)
 
 emp_2 = Employee("Wenhong", "Hu", 8888)
 print("Employee 2: ", emp_2.fullname())
@@ -48,11 +63,31 @@ Employee.set_raise_amt(0.99)
 print("New raise amount for all emps", Employee.raise_amount)
 emp_3 = Employee("Tony", "Cai", 9999)
 print("Adjust raise for new emp_3", emp_3.raise_amount)
-print("Adjust 3rd raise for emp_1", emp_1.raise_amount) # not changing because of setting by instance not class
+print("Adjust 3rd raise for emp_1", emp_1.raise_amount) # raise_amount not changing because of setting by instance not class
 print("Adjust raise for emp_2", emp_2.raise_amount)
-print(f"Look all right, so apply raise: \nemp_1: {emp_1.apply_raise()} \
-                                        \nemp_2: {emp_2.apply_raise()} \
-                                        \nemp_3: {emp_3.apply_raise()}\n")
+emp_1.apply_raise()
+emp_2.apply_raise()
+emp_3.apply_raise()
+print(f"Look all right, so apply raise: \nemp_1: {emp_1.pay} \
+                                        \nemp_2: {emp_2.pay} \
+                                        \nemp_3: {emp_3.pay}\n")
 emp_str_11 = 'Jane-Dow-6000'
 new_employee_11 = Employee.from_string(emp_str_11)
 print("New employee 11:", new_employee_11.fullname(), new_employee_11.email, new_employee_11.pay)
+
+print("====================================================================================\n")
+# use staticmethod
+date_1 = datetime.date(2021, 4, 1) # Thr
+date_2 = datetime.date(2021, 4, 4) # Sun
+print("Go to work? ", Employee.is_worday(date_1))
+print("Go to work? ", Employee.is_worday(date_2))
+
+print("====================================================================================\n")
+# inheritance
+dev_1 = Developer('DT', 'Wu', 88000)
+dev_2 = Developer('DJ', 'Song', 99000)
+print(dev_1.email, dev_2.email)
+
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
