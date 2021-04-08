c0894d79a3231caefb8634890eaae15c0f3b6c02
Yichen <msdaiyichen@gmail.com>
2021-04-08 00:29:04 -0700

    Clean commented out codes

--- traffic_light_controller.py
+++ traffic_light_controller.py
@@ -71,8 +71,6 @@ class TrafficLight:
         carId: int,                      # ID of the car
         roadId: int,                     # ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
         direction: int,                  # Direction of the car
-        # turnGreen: 'Callable[[], None]', # Use turnGreen() to turn light to green on current road
-        # crossCar: 'Callable[[], None]'   # Use crossCar() to make car cross the intersection
     ) -> None:
         if self.prev_roadId != roadId:
             self.turnGreen()     
